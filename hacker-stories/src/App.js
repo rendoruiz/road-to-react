b2d375c942be6e2b46460c81e1e53e763a9b34d7
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-09 16:09:27 -0700

    Update App.js

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,35 +1,37 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React', 
-    url: 'https://reactjs.org/', 
-    author: 'Jordan Walke', 
-    num_comments: 3, 
-    points: 4, 
-    objectID: 0,
-  }, 
-  {
-    title: 'Redux', 
-    url: 'https://redux.js.org/', 
-    author: 'Dan Abramov, Andrew Clark', 
-    num_comments: 2, 
-    points: 5, 
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: 'React', 
+      url: 'https://reactjs.org/', 
+      author: 'Jordan Walke', 
+      num_comments: 3, 
+      points: 4, 
+      objectID: 0,
+    }, 
+    {
+      title: 'Redux', 
+      url: 'https://redux.js.org/', 
+      author: 'Dan Abramov, Andrew Clark', 
+      num_comments: 2, 
+      points: 5, 
+      objectID: 1,
+    },
+  ];
+  
+  return (
+    <div className="App">
+      <h1>My Hacker Stories</h1>
 
-const App = () => (
-  <div className="App">
-    <h1>My Hacker Stories</h1>
+      <Search />
 
-    <Search />
+      <hr />
 
-    <hr />
-
-    <List list={list} />
-  </div>
-);
+      <List list={stories} />
+    </div>
+  );
+}
 
 const List = (props) => ( 
   <ul>
@@ -50,11 +52,17 @@ const Item = (props) => (
   </li> 
 );
 
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label> 
-    <input id="search" type="text" />
-  </div>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label> 
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+}
 
 export default App;
