8c6a89a4d9dce9f7d2fe3bef5e3b250795b765d4
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-09 17:00:34 -0700

    handling side effects using useeffect hook

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -20,12 +20,18 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
-  }
-
+  };
+    
   const searchedStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm)
   );
