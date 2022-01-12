6015e12142d51972a9014289424d71fdad251fb0
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-12 16:57:04 -0700

    reducer, useReducer

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -12,6 +12,20 @@ const useSemiPersistentState = (key, initialState) => {
   return [value, setValue];
 }
 
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(
+        (story) => action.payload.objectID !== story.objectID
+      );
+    default:
+      throw new Error();
+  }
+};
+  
+
 const App = () => {
   const initialStories = [
     {
@@ -40,9 +54,13 @@ const App = () => {
       )
     );
 
-
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
-  const [stories, setStories] = React.useState([]);
+  
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    []
+  );
+    
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
 
@@ -51,18 +69,20 @@ const App = () => {
     setIsLoading(true);
 
     getAsyncStories().then(result => {
-      setStories(result.data.stories);
+      dispatchStories({
+        type: 'SET_STORIES',
+        payload: result.data.stories,
+      });
       setIsLoading(false);
     })
     .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-
-    setStories(newStories);
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
   }
 
   const handleSearch = (event) => {
