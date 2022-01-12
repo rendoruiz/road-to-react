167a327b25fe7ff0a125b43cc0cc66db434278fe
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-12 14:48:45 -0700

    children prop

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -48,11 +48,11 @@ const App = () => {
 
       <InputWithLabel  
         id="search"
-        label="search"
         value={searchTerm} 
         onInputChange={handleSearch}
-      />
-
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
       <hr />
 
       <List list={searchedStories} />
@@ -79,9 +79,10 @@ const Item = ({ item }) => (
   </li> 
 );
 
-const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => (
+const InputWithLabel = ({ id, value, type = 'text', onInputChange, children }) => (
   <>
-    <label htmlFor={id}>{label}{label.split('').pop() !== ":" && ":"}</label> 
+    <label htmlFor={id}>{children}</label> 
+    &nbsp;
     <input 
       id={id} 
       type={type} 
