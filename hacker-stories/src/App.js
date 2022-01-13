577117e05d7f9d338daf4e4d3c2ffe0cd8c2786a
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-13 14:18:59 -0700

    replace fetch with axios

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import axios from 'axios';
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -64,12 +65,12 @@ const App = () => {
   const handleFetchStories = React.useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() => 
