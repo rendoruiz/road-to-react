e2bf4688e3d7080560affafa8bc299e914d37b46
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-01-13 14:25:42 -0700

    refactor form

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -92,30 +92,21 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
   };
   
   return (
     <div className="App">
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel  
-        id="search"
-        value={searchTerm} 
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
       <hr />
 
       {stories.isError && <p>Something went wrong ...</p>}
@@ -158,6 +149,26 @@ const Item = ({ item, onRemoveItem }) => (
       </button>
     </span>
   </li> 
+);
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
 );
 
 const InputWithLabel = ({ 
